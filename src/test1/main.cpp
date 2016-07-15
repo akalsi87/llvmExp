6d8f4f129ff547a28340fd46f1f1a1f5e7bc0a9d
Aaditya Kalsi <akalsi.dev@gmail.com>
2016-07-14 21:58:15 -0400

    "Improve test1"

--- src/test1/main.cpp
+++ src/test1/main.cpp
@@ -7,17 +7,40 @@
 #define __STDC_CONSTANT_MACROS
 
 #include "llvm/AsmParser/Parser.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Verifier.h"
+
+#include <iostream>
 
 llvm::SMDiagnostic SM_DIAG;
 llvm::LLVMContext CTXT;
 
 int main(int argc, char** argv)
 {
+    std::string s;
+    llvm::raw_string_ostream strm(s);
+
     auto mod = llvm::parseAssemblyFile(argv[1], SM_DIAG, CTXT);
-    mod->dump();
+
+    if (!mod)
+    {
+        SM_DIAG.print(argv[0], llvm::errs());
+        std::cerr << strm.str() << std::endl;
+        return 1;
+    }
+
+    if (llvm::verifyModule(*mod))
+    {
+        llvm::verifyModule(*mod, &strm);
+        std::cerr << strm.str() << std::endl;
+        return 1;
+    }
+
+    mod->print(strm, nullptr);
+    std::cout << strm.str() << std::endl;
+
     return 0;
 }
